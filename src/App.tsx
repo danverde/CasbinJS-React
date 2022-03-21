b6924d9d65be6a31f6163aceb2422dbb317466a4
Daniel Green <dgreen@docutechcorp.com>
2022-03-21 15:50:47 -0600

    cleanup app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import casbinService from './CasbinService';
 
@@ -13,25 +12,9 @@ function App() {
   const isAllowed: boolean = casbinService.enforce('alice', '*', 'read');
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <main>
-        <h1>{isAllowed}</h1>
-      </main>
-    </div>
+    <main>
+      <h1>isAllowed: {isAllowed.toString()}</h1>
+    </main>
   );
 }
 
